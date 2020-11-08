c79d92317f8212256747f4403502f0ce1d34e927
bbartman <bbartman@kent.edu>
2020-11-08 09:07:12 -0500

    Started figuring out how to create the drop down and screen that I want to display.

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.uix.widget import Widget
+from kivy.uix.dropdown import DropDown
+from kivy.uix.button import Button
+from kivy.uix.gridlayout import GridLayout
 from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
 
 class MainScreen(Screen):
@@ -26,7 +30,53 @@ class RunDeathmatchScreen(Screen):
 class ScreenManagement(ScreenManager):
     pass
 
+class DoorDropScreenManager(ScreenManager):
+    pass
+
+class DD_NoDropScreen(Screen):
+    pass
+
+class DD_AlwaysOpenScreen(Screen):
+    pass
+
+class DD_DropAfterTimeScreen(Screen):
+    pass
+
+class CustomDropDown(DropDown): 
+    pass
+
+class DropdownDemo(GridLayout): 
+    '''The code of the application itself.''' 
+    def __init__(self, **kwargs): 
+          
+        '''The button at the opening of the window is created here, 
+        not in kv 
+        ''' 
+        super(DropdownDemo, self).__init__(**kwargs) 
+        self.dropdown = CustomDropDown() 
+  
+        # Creating a self widget bouton 
+        self.mainbutton = Button(text ='Do you in college?', 
+                                 size_hint_x = 0.6, size_hint_y = 0.15) 
+          
+        # Added button to FloatLayout so inherits this class  
+        self.add_widget(self.mainbutton) 
+  
+        # Adding actions  
+        # If click  
+        self.mainbutton.bind(on_release = self.dropdown.open) 
+  
+        # root.select on_select called 
+        self.dropdown.bind(on_select = lambda instance, x: setattr(self.mainbutton, 'text', x)) 
+        self.dropdown.bind(on_select = self.callback) 
+   
+    def callback(self, instance, x): 
+        '''x is self.mainbutton.text refreshed''' 
+        print ( "The chosen mode is: {0}" . format ( x ) ) 
+
+
 presentation = Builder.load_file("main.kv")
+
 
 class MainApp(App):
     def build(self):
