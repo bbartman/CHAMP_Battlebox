54f07c6e21ee2357838e4c3b2eba6c6e9dd4bba4
bbartman <bbartman@kent.edu>
2020-11-11 17:38:38 -0500

    Finished up the basic menus.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from kivy.lang import Builder
 from kivy.uix.widget import Widget
 from kivy.uix.dropdown import DropDown
 from kivy.uix.button import Button
+from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
 
@@ -19,6 +20,9 @@ class BattleRoyaleScreen(Screen):
     pass
 
 class SoccerScreen(Screen):
+    pass
+
+class RunSoccerScreen(Screen):
     pass
 
 class TournamentScreen(Screen):
@@ -46,29 +50,20 @@ class CustomDropDown(DropDown):
     pass
 
 class DropdownDemo(GridLayout): 
-    '''The code of the application itself.''' 
     def __init__(self, **kwargs): 
-          
-        '''The button at the opening of the window is created here, 
-        not in kv 
-        ''' 
         super(DropdownDemo, self).__init__(**kwargs) 
+        self.labelDispalay = Label(text='Door Drop Mode', font_size=30)
+        self.add_widget(self.labelDispalay) 
         self.dropdown = CustomDropDown() 
-  
-        # Creating a self widget bouton 
-        self.mainbutton = Button(text ='Do you in college?', 
-                                 size_hint_x = 0.6, size_hint_y = 0.15) 
-          
-        # Added button to FloatLayout so inherits this class  
+        self.mainbutton = Button(text ='Drop Both')
+
+        # Added button to FloatLayout so inherits this class
         self.add_widget(self.mainbutton) 
-  
-        # Adding actions  
-        # If click  
         self.mainbutton.bind(on_release = self.dropdown.open) 
   
         # root.select on_select called 
-        self.dropdown.bind(on_select = lambda instance, x: setattr(self.mainbutton, 'text', x)) 
-        self.dropdown.bind(on_select = self.callback) 
+        self.dropdown.bind(on_select = lambda instance, x: setattr(self.mainbutton, 'text', x))
+        self.dropdown.bind(on_select = self.callback)
    
     def callback(self, instance, x): 
         '''x is self.mainbutton.text refreshed''' 
